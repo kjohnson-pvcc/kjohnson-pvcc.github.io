753c872f6559146c1cf55cf0d182aa2358ca1c8f
kjohnson-pvcc <125826734+kjohnson-pvcc@users.noreply.github.com>
2023-04-27 22:14:49 -0400

    Add files via upload

--- pay.py
+++ pay.py
@@ -2,21 +2,22 @@
 #Prog Purpose: This program calculates the pay of each employee
 
 import datetime
-# define hourly pay rates
-HOURLY_PAY_RATES = (16.50, 15.75, 15.75, 19.50)
-
-# define deduction rates
+# define pay rates and tax rates
+HOURLY_PAY_RATE = (16.50, 15.75, 15.75, 19.50)
 DEDUCTION_RATES = (.12, .03, .062, .0145)
 
 # define global variables
-job = 1 # 1 means Cashier 2 means Stocker 3 means Janitor 4 means Maintenance
+job = 1 # 1 means Cashier
+job = 2 # 2 means Stocker
+job = 3 # 3 means Janitor
+job = 4 # 4 means Maintenance
 numhours = 0
-grosspay = 0
-fed_rate = 0
-state_rate = 0
-ss_rate = 0
-med_rate = 0
-tot_ded = 0
+grosspay = ()
+fed_tax = 0
+state_tax = 0
+ss_tax = 0
+med_tax = 0
+tot_ded = fed_tax + state_tax + ss_tax + med_tax
 netpay = 0
  
 ############### define program function #################
@@ -37,22 +38,25 @@ def get_user_data():
     numhours = int(input("Enter the number of hours the employee worked for the week."))
                    
 def perform_calculations():
-    global grosspay, fed_rate, state_rate, ss_rate, med_rate, tot_ded, netpay
-    if job[0] == 1:
-        grosspay = numhours * HOURLY_PAY_RATES
-    if job[1] == 2:
-        grosspay = numhours * HOURLY_PAY_RATES
-    if job[2] == 3:
-        grosspay = numhours * HOURLY_PAY_RATES
-    if job[3] == 4:
-        grosspay = numhours * HOURLY_PAY_RATES
+    global grosspay, fed_tax, state_tax, ss_tax, med_tax, HOURLY_PAY_RATE, DEDUCTION_RATES, tot_ded, netpay
+    if job == 1:
+        grosspay = numhours * HOURLY_PAY_RATE[0]
+        
+    if job == 2:
+        grosspay = numhours * HOURLY_PAY_RATE[1]
+        
+    if job == 3:
+        grosspay = numhours * HOURLY_PAY_RATE[2]
+        
+    if job == 4:
+        grosspay = numhours * HOURLY_PAY_RATE[3]
 
-    fed_rate = grosspay * DEDUCTION_RATES
-    state_rate = grosspay * DEDUCTION_RATES
-    ss_rate = grosspay * DEDUCTION_RATES
-    med_rate = grosspay * DEDUCTION_RATES
-    tot_ded = fed_rate + state_rate + ss_rate + med_rate
-    netpay = grosspay - tot_ded
+        fed_tax = grosspay * DEDUCTION_RATES[0]
+        state_tax = grosspay * DEDUCTION_RATES[1]
+        ss_tax = grosspay * DEDUCTION_RATES[2]
+        med_tax = grosspay * DEDUCTION_RATES[3]
+        tot_ded = fed_tax + state_tax + ss_tax + med_tax
+        netpay = grosspay - tot_ded
     
 def display_results():
     print('------------------------------------------')
@@ -60,16 +64,16 @@ def display_results():
     print('------------------------------------------')
     print('Date Range                                ')
     print('Employee Name                             ')
-    print('Num Hours Worked        ' + str(numhours)) 
+    print('Num Hours Worked               ' + str(numhours)) 
     print('Gross Pay             $ ' + format(grosspay,'8,.2f'))
-    print('Federal Income Tax    $ ' + format(fed_rate,'8,.2f'))
-    print('State Income Tax      $ ' + format(state_rate,'8,.2f'))
-    print('Social Security Tax   $ ' + format(ss_rate,'8,.2f'))
-    print('Medicare Tax          $ ' + format(med_rate,'8,.2f'))
-    print('Total Deductions      $ ' + format(tot_ded,'8,.2f'))
+    print('Federal Income Tax    $ ' + format(DEDUCTION_RATES[0]*grosspay,'8,.2f'))
+    print('State Income Tax      $ ' + format(DEDUCTION_RATES[1]*grosspay,'8,.2f'))
+    print('Social Security Tax   $ ' + format(DEDUCTION_RATES[2]*grosspay,'8,.2f'))
+    print('Medicare Tax          $ ' + format(DEDUCTION_RATES[3]*grosspay,'8,.2f'))
+    print('Total Deductions      $ ' + format(fed_tax + state_tax + ss_tax + med_tax,'8,.2f'))
     print('------------------------------------------')
-    print('Net Pay               $ ' + format(netpay,'8,.2f'))
+    print('Net Pay               $ ' + format(grosspay - tot_ded,'8,.2f'))
     print(str(datetime.datetime.now()))
-  
+ 
 ########## call on main program to execute #############
 main()    
